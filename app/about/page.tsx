5afe909e1559398830ebd16e36d9db018a522dde
Indi Rinearson <indi.rinearson@gmail.com>
2026-06-24 10:49:15 -0700

    scaffold about/projects page structures

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,11 @@
 const AboutPage = () => {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center font-sans">
-      <h1 className="text-pale-slate">About</h1>
+    <div className="flex flex-col lg:flex-row flex-1 items-center justify-center font-sans gap-4">
+      <div className="bg-periwinkle rounded-full h-52 px-2 flex justify-center items-center">
+        Placeholder for image
+      </div>
+
+      <div className="text-pale-slate">Placeholder for bio</div>
     </div>
   );
 };
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,6 +2,10 @@ const ProjectsPage = () => {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans">
       <h1 className="text-pale-slate">Projects</h1>
+
+      <div className="bg-olive-wood h-44 flex justify-center items-center p-4 absolute bottom-8 right-12">
+        Placeholder for graphic
+      </div>
     </div>
   );
 };

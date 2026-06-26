a8eb669b990d9d32a1403cbdec8f520e4c936576
Indi Rinearson <indi.rinearson@gmail.com>
2026-06-25 19:32:18 -0700

    add anvil graphic

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,17 +1,25 @@
+import Image from "next/image";
 import { ProjectCard, type Project } from "@/components/project-card";
+import Anvil from "@/public/anvil.png";
 
 const ProjectsPage = () => {
   return (
-    // INDI: Confirm this pb once the actual graphic is added
-    <div className="relative flex flex-col flex-1 w-full items-center p-6 pb-56">
-      <div className="relative grid w-full grid-cols-1 md:grid-cols-2 gap-8 md:gap-20 md:px-16 lg:px-24 xl:px-32 overflow-x-auto justify-items-center">
+    <div className="relative flex flex-col flex-1 w-full items-center p-6 pb-96">
+      <div className="relative z-10 grid w-full grid-cols-1 md:grid-cols-2 gap-8 md:gap-20 md:px-16 lg:px-24 xl:px-32 overflow-x-auto justify-items-center">
         {PROJECTS.map((project, idx) => (
           <ProjectCard project={project} key={idx} />
         ))}
       </div>
 
-      <div className="absolute bottom-8 right-8 flex h-44 items-center justify-center bg-olive-wood p-4">
-        Placeholder for graphic
+      <div
+        className="pointer-events-none fixed bottom-4 right-0 z-20 md:p-4"
+        aria-hidden
+      >
+        <Image
+          alt="Pixel of a medieval blacksmith's anvil"
+          src={Anvil}
+          height={320}
+        />
       </div>
     </div>
   );
